09052b356963659374b3982276c31bd04c084004
Thiago Scalone <thiago@scalone.com.br>
2017-08-08 13:22:39 -0300

    Implement Kernel#print_last

--- lib/ext/kernel.rb
+++ lib/ext/kernel.rb
@@ -6,4 +6,8 @@ module Kernel
   def print_line(buf, row = 0, column = 0)
     Device::Display.print_line(buf, row, column)
   end
+
+  def print_last(buf)
+    Device::Display.print_line(buf, STDOUT.max_y - 1, 0)
+  end
 end
