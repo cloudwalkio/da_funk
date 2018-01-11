522491cb9d5f7cb754b75e58c975c68a53bfa8c7
Thiago Scalone <thiago@scalone.com.br>
2018-01-11 15:50:24 -0200

    Refactoring NotificationEventTest following the implementation

--- test/unit/device/notification_event_test.rb
+++ test/unit/device/notification_event_test.rb
@@ -1,73 +1,29 @@
 
 class NotificationEventTest < DaFunk::Test.case
   def setup
-    @stream_event = {
-      "Coalesce" => true,
-      "LTime" => 1792, 
-      "Payload" => "{\"Body\"=>\"message 62\"}",
-      "Name" => "pc1;0101",
-      "Event" => "user"
-    }
-    @event = DaFunk::NotificationEvent.new(@stream_event)
+    @str = '{"id":3,"event":"3|SHOW_MESSAGE|TEST1|12-20-2017 18:23","acronym":"pc1","logical_number":"5A179615"}'
+    @json  = JSON.parse @str
+    @event = DaFunk::NotificationEvent.new(@json["id"], @json["event"], @json["acronym"], @json["logical_number"])
   end
 
-  def test_attr_coalesce
-    assert_equal @stream_event["Coalesce"], @event.coalesce
+  def test_attr_id
+    assert_equal @json["id"], @event.id
   end
 
-  def test_attr_ltim
-    assert_equal @stream_event["LTime"], @event.ltime
+  def test_attr_acronym
+    assert_equal @json["acronym"], @event.acronym
   end
 
-  def test_attr_payload
-    assert_equal @stream_event["Payload"], @event.payload
+  def test_attr_logical_number
+    assert_equal @json["logical_number"], @event.logical_number
   end
 
-  def test_attr_name
-    assert_equal @stream_event["Name"], @event.name
-  end
-
-  def test_attr_event
-    assert_equal @stream_event["Event"], @event.event
-  end
-
-  def test_attr_message
-    assert_equal "{\"Body\" : \"message 62\"}", @event.message
-  end
-
-  def test_attr_value
-    hash = {"Body" => "message 62"}
-    assert_equal hash, @event.value
-  end
-
-  def test_attr_value
-    assert_equal "message 62", @event.body
+  def test_attr_parameters
+    assert_equal ["TEST1", "12-20-2017 18:23"], @event.parameters
   end
 
   def test_attr_callback
-    assert_equal "message 62", @event.callback
-  end
-
-  def test_attr_callback
-    assert_equal "message 62", @event.callback
-  end
-
-  def test_attr_none_parameters
-    assert_equal [], @event.parameters
-  end
-
-  def test_attr_one_parameter
-    stream_event = @stream_event.dup
-    stream_event["Payload"].gsub!("a", "|")
-    event = DaFunk::NotificationEvent.new(stream_event)
-    assert_equal ["ge 62"], event.parameters
-  end
-
-  def test_attr_multiple_parameters
-    stream_event = @stream_event.dup
-    stream_event["Payload"].gsub!("e", "|")
-    event = DaFunk::NotificationEvent.new(stream_event)
-    assert_equal ["ssag", " 62"], event.parameters
+    assert_equal "SHOW_MESSAGE", @event.callback
   end
 end
 
