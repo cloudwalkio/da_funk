ef8b26ef69b88f3958e2373590e563e33c9b2892
Thiago Scalone <thiago@scalone.com.br>
2018-01-11 15:48:36 -0200

    Implement Float.to_s with other bases 10, nil and others

--- lib/ext/float.rb
+++ lib/ext/float.rb
@@ -1,7 +1,7 @@
 class Float
-  alias to_s_old to_s
   def to_s(*args)
-    if args.first == 16
+    case args.first
+    when 16
       t = self
       v = []
       loop do
@@ -10,9 +10,39 @@ class Float
         break if t == 0
       end
       v.reverse.inject("") {|str, v| str << v.to_i.to_s(16)}
+    when 10, nil
+      if self.to_i.is_a?(Fixnum)
+        value = ("%f" % self)
+      else
+        t = self
+        v = []
+        loop do
+          v << (t % 10).to_i
+          t =  (t / 10).to_i
+          break if t == 0
+        end
+        value = v.reverse.inject("") {|str, v| str << v.to_i.to_s}
+      end
+      remove_zero(value)
     else
-      to_s_old(*args)
+      if self.to_i.is_a?(Fixnum)
+        self.to_i.to_s(*args)
+      else
+        "%f" % self
+      end
     end
+  end
+
+  def remove_zero(str)
+    exclude = true
+    str.reverse.chars.select do |v|
+      if (v == "0" || v == ".") && exclude
+        false
+      else
+        exclude = false
+        true
+      end
+    end.reverse.join
   end
 end
 
