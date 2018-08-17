89920aa2dd91a46c830c147774f9548656c330f1
Thiago Scalone <thiago@scalone.com.br>
2018-08-17 17:24:02 -0300

    Support schedule events in file if using hours parameters

--- lib/da_funk/event_handler.rb
+++ lib/da_funk/event_handler.rb
@@ -24,19 +24,39 @@ module DaFunk
 
     def schedule_timer
       if option.is_a?(Hash)
-        if option.include?(:minutes)
-          timer = Time.now + (option[:minutes].to_i * 60)
+        if option.include?(:hours) && option.include?(:slot)
+          self.timer = Time.at(seconds_from_file)
+        elsif option.include?(:minutes)
+          self.timer = Time.now + (option[:minutes].to_i * 60)
         elsif option.include?(:seconds)
-          timer = Time.now + option[:seconds]
+          self.timer = Time.now + option[:seconds]
         end
       end
     end
 
+    def seconds_from_file
+      unless option[:slot] && option[:hours]
+        raise "slot or hours missing on EventHandler creation"
+      end
+
+      db    = FileDb.new("main/schedule.dat")
+      value = db[option[:slot]]
+
+      if value.to_s.empty? || value.to_i <= Time.now.to_i
+        hours = 60 * 60 * option[:hours].to_i
+        hours = 99_999 if hours == 0
+        value = (Time.now.to_i + hours)
+        db[option[:slot]] = value
+      end
+      value.to_i
+    end
+
     def execute?
-      if timer
-        timer < Time.now
+      schedule_timer unless self.timer
+      if self.timer
+        self.timer < Time.now
       else
-        true
+        ! self.timer
       end
     end
   end
