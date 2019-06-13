a08a32d03a44e9ab0aa15a5406d4e869e720f232
Thiago Scalone <thiago@scalone.com.br>
2019-06-13 18:35:22 -0300

    Fix ISO8583 binary bitmap generation to CRUBY

--- lib/iso8583/bitmap.rb
+++ lib/iso8583/bitmap.rb
@@ -59,21 +59,25 @@ module ISO8583
 
     # Generate the bytes representing this bitmap.
     def to_bytes
-      # Convert binary to hex, by slicing the binary in 4 bytes chuncks
-      bitmap_hex = ""
-      str = ""
-      self.to_s.chars.reverse.each_with_index do |ch, i|
-        str << ch
-        next if i == 0
-        if (i+1) % 4 == 0
-          bitmap_hex << str.reverse.to_i(2).to_s(16)
-          str = ""
+      if RUBY_ENGINE == 'mruby'
+        # Convert binary to hex, by slicing the binary in 4 bytes chuncks
+        bitmap_hex = ""
+        str = ""
+        self.to_s.chars.reverse.each_with_index do |ch, i|
+          str << ch
+          next if i == 0
+          if (i+1) % 4 == 0
+            bitmap_hex << str.reverse.to_i(2).to_s(16)
+            str = ""
+          end
         end
+        unless str.empty?
+          bitmap_hex << str.reverse.to_i(2).to_s(16)
+        end
+        bitmap_hex.reverse.upcase
+      else
+        [to_s].pack("B*")
       end
-      unless str.empty?
-        bitmap_hex << str.reverse.to_i(2).to_s(16)
-      end
-      bitmap_hex.reverse.upcase
     end
     alias_method :to_b, :to_bytes
 
