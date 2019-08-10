b24ab6bb2edfeec630649d47c154bd3937804459
Thiago Scalone <thiago@scalone.com.br>
2019-08-10 14:06:31 -0300

    Force encoding of mti value on ISO8583 message to_b

--- lib/iso8583/message.rb
+++ lib/iso8583/message.rb
@@ -179,7 +179,7 @@ module ISO8583
     # Retrieve the byte representation of the bitmap.
     def to_b
       raise ISO8583Exception.new "no MTI set!" unless mti
-      mti_enc = self.class._mti_format.encode(mti)
+      mti_enc = self.class._mti_format.encode(mti).force_encoding('UTF-8')
       mti_enc << _body.join
     end
 
