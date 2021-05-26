1ca7c0905124edfc627f3a94b149bbaa2049ef74
Bruno Coimbra <bruno.coimbra@cloudwalk.io>
2021-05-25 22:19:00 -0300

    remove `puts` statement (it was for debug); require `byebug`

--- test/iso8583/minitest_helper.rb
+++ test/iso8583/minitest_helper.rb
@@ -3,9 +3,9 @@ if ENV['COV']
   SimpleCov.start
 end
 
+require 'byebug'
 require 'minitest/autorun'
 PROJECT_ROOT = File.join(File.dirname(File.expand_path(__FILE__)), '..', '..')
 Dir.glob(File.join(PROJECT_ROOT, 'lib', 'iso8583', '**', '*.rb')).each do |file|
-  puts file
   require file
 end
