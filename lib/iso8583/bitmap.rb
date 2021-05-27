a386f5218c0ac496ecdf209569fbe50126eab444
Bruno Coimbra <bruno.coimbra@cloudwalk.io>
2021-05-27 16:06:58 -0300

    Add steroids on Bitmap
    * Support different sizes of bitmaps (has to be multiple of 8)
    * Support dynamic quantity of bitmaps (i.e. primary, secondary, tertiary, ..., bitmaps)
    * Fix parser to support both previous features
    * Cover with tests

--- lib/iso8583/bitmap.rb
+++ lib/iso8583/bitmap.rb
@@ -15,25 +15,48 @@ module ISO8583
   class Bitmap
     include DaFunk::Helper
 
+    class << self
+      # Parse the bytes in string and return the Bitmap and bytes remaining in `str`
+      # after the bitmap is taken away.
+      def parse(str, hex_bitmap = false, bitmap_size: 64)
+        bmp  = Bitmap.new(str, hex_bitmap, bitmap_size: bitmap_size)
+
+        rest = if bmp.hex_bitmap?
+                 str[bmp.size_in_bytes_hex, str.length]
+               else
+                 str[bmp.size_in_bytes, str.length]
+               end
+
+        [ bmp, rest ]
+      end
+    end
+
     # bitmap_size defines the size in bits of bitmap. It has to be multiple of 8 (a byte of 8 bits)
     attr_reader :bitmap_size
 
-    # has_additional_bitmap defines if the bit 1 (left to right) indicates the presence of another
+    # additional_bitmap defines if the bit 1 (left to right) indicates the presence of another
     # bitmap just after the current one
-    attr_reader :has_additional_bitmap
+    attr_reader :additional_bitmap
+
+    attr_reader :bitmaps
 
     # create a new Bitmap object. In case an iso message
     # is passed in, that messages bitmap will be parsed. If
     # not, this initializes and empty bitmap.
-    def initialize(message = nil, hex_bitmap=false, bitmap_size: 128, has_additional_bitmap: true)
+    def initialize(message = nil, hex_bitmap=false, bitmap_size: 128, additional_bitmap: true)
       raise ISO8583Exception.new "wrong bitmap_size: #{bitmap_size}" if bitmap_size % 8 != 0
 
       @bitmap_size           = bitmap_size
       @bmp                   = Array.new(bitmap_size, false)
       @hex_bitmap            = hex_bitmap
-      @has_additional_bitmap = has_additional_bitmap
+      @additional_bitmap = additional_bitmap
+      @bitmaps               = 1
 
       message ? initialize_from_message(message) : nil
+    end
+
+    def additional_bitmap?
+      !!@additional_bitmap
     end
 
     def hex_bitmap?
@@ -50,32 +73,34 @@ module ISO8583
       @bmp[i-1]
     end
 
-    # Set the bit to the indicated value. Only `true` sets the
-    # bit, any other value unsets it.
-    # TODO @bruno.coimbra -- This method should be private
-    def []=(i, value)
-      if i > bitmap_size
-        raise ISO8583Exception.new("Bits > #{bitmap_size} are not permitted. bitmap_size == #{bitmap_size}")
-      elsif i < 2
-        if has_additional_bitmap
-          raise ISO8583Exception.new("Bits < 2 are not permitted (continuation bit is set automatically)")
-        end
-      end
-      @bmp[i-1] = (value == true)
-    end
-
     # Sets bit #i
     def set(i)
-      if i > bitmap_size
-        raise ISO8583Exception, "can't set field #{i}, bitmap_size == #{bitmap_size}" unless has_additional_bitmap
+      if additional_bitmap?
+        raise ISO8583Exception, "field #{i} shouldn't be set (continuation bit is set automatically)" if i % bitmap_size == 1
+      end
 
+      if i > bitmap_size
+        raise ISO8583Exception, "can't set field #{i}, bitmap_size == #{bitmap_size}" unless additional_bitmap?
+
+        quo = i / bitmap_size
         rem = i % bitmap_size
-        @bitmap_size = i + rem
-        new_bmp = Array.new(bitmap_size, false)
-        @bmp.each_with_index {|v, i| new_bmp[i] = v}
+        @bitmaps = rem > 0 ? quo + 1 : quo
+        new_bmp = Array.new(@bitmaps * bitmap_size, false)
+        @bmp.each_with_index { |v, i| new_bmp[i] = v }
+        0.upto(@bitmaps - 2) do |pos|
+          new_bmp[pos * bitmap_size] = true
+        end
         @bmp = new_bmp
       end
       self[i] = true
+    end
+
+    def size_in_bytes
+      bitmaps * (bitmap_size / 8)
+    end
+
+    def size_in_bytes_hex
+      size_in_bytes * 2
     end
 
     # Unsets bit #i
@@ -88,13 +113,13 @@ module ISO8583
       if RUBY_ENGINE == 'mruby'
         # Convert binary to hex, by slicing the binary in 4 bytes chuncks
         bitmap_hex = ""
-        str = ""
+        str = String.new("", encoding: 'ASCII-8BIT')
         self.to_s.chars.reverse.each_with_index do |ch, i|
           str << ch
           next if i == 0
           if (i+1) % 4 == 0
             bitmap_hex << str.reverse.to_i(2).to_s(16)
-            str = ""
+            str = String.new("", encoding: 'ASCII-8BIT')
           end
         end
         unless str.empty?
@@ -102,28 +127,21 @@ module ISO8583
         end
         bitmap_hex.reverse.upcase
       else
-        [to_s].pack("B*").force_encoding('UTF-8')
+        [to_s].pack("B*")
       end
     end
     alias_method :to_b, :to_bytes
 
     def to_hex
-      value = self.to_s.to_i(2).to_s(16).upcase
-      if value.respond_to? :force_encoding
-        value.force_encoding('UTF-8')
-      else
-        value
-      end
+      self.to_s.to_i(2).to_s(16).upcase.ljust(size_in_bytes_hex, '0')
     end
 
     # Generate a String representation of this bitmap in the form:
     #	01001100110000011010110110010100100110011000001101011011001010
     def to_s
-      #check whether any `high` bits are set
-      ret           = (65..128).find {|bit| !!self[bit]}
-      high, @bmp[0] = ret ? [128, true] : [64, false]
+      high = @bitmaps * @bitmap_size
 
-      str = ""
+      str = String.new("", encoding: "ASCII-8BIT")
       1.upto(high) do|i|
         str << (self[i] ? '1' : '0')
       end
@@ -134,6 +152,12 @@ module ISO8583
 
     private
 
+    # Set the bit to the indicated value. Only `true` sets the
+    # bit, any other value unsets it.
+    def []=(i, value)
+      @bmp[i-1] = (value == true)
+    end
+
     def convert_hex_to_binary(str)
       str.chars.reverse.inject("") do |string, ch|
         string + ch.to_i(16).to_s(2).rjust(4, "0").reverse
@@ -142,40 +166,30 @@ module ISO8583
 
     def initialize_from_message(message)
       bmp = if hex_bitmap?
-              rjust(convert_hex_to_binary(message[0..15]), 64, '0')
+              slice_range = (bitmap_size * 2) / 8 - 1
+              rjust(convert_hex_to_binary(message[0..slice_range]), bitmap_size, '0')
             else
-              message.unpack("B64")[0]
+              message.unpack("B#{bitmap_size}")[0]
             end
 
-      if bmp[0,1] == "1"
-        bmp = if hex_bitmap?
-                rjust(convert_hex_to_binary(message[0..31]), 128, '0')
-              else
-                message.unpack("B128")[0]
-              end
+      if additional_bitmap?
+        has_next_bitmap = bmp[0] == "1"
+        while has_next_bitmap
+          @bitmaps += 1
+          bmp = if hex_bitmap?
+                  slice_range = (bitmap_size * bitmaps * 2) / 8 - 1
+                  rjust(convert_hex_to_binary(message[0..slice_range]), bitmap_size * bitmaps, '0')
+                else
+                  message.unpack("B#{bitmap_size * bitmaps}")[0]
+                end
+          has_next_bitmap = bmp[bitmap_size * (bitmaps - 1)] == "1"
+        end
       end
 
       0.upto(bmp.length-1) do |i|
         @bmp[i] = (bmp[i,1] == "1")
       end
     end
-
-    class << self
-      # Parse the bytes in string and return the Bitmap and bytes remaining in `str`
-      # after the bitmap is taken away.
-      def parse(str, hex_bitmap = false)
-        bmp  = Bitmap.new(str, hex_bitmap)
-
-        rest = if bmp.hex_bitmap?
-                 bmp[1] ? str[32, str.length] : str[16, str.length]
-               else
-                 bmp[1] ? str[16, str.length] : str[8, str.length]
-               end
-
-        [ bmp, rest ]
-      end
-    end
-
   end
 end
 
